38bc1f02ba1c90ae965657689d801526d5373c6f
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-02-02 09:52:01 +0000

    [Update]検索の機能完成(表示未完成)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,33 +1,36 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
 
 def search
-  kind = params[:kind]
+  @kind = params[:kind]
   match_type = params[:match_type]
-  keyword = params[:keyword]
-  if kind == 'book'
+  @keyword = params[:keyword]
+  if @kind == 'book'
     case match_type
     when 'perfect_match' then
-      @books = Book.where(title: keyword)
+      @books = Book.where("title LIKE ?", "#{@keyword}")
     when 'forward_match' then
-      @books = Book.where("title LIKE ?", "%#{keyword}")
+      @books = Book.where("title LIKE ?", "%#{@keyword}")
     when 'backword_match' then
-      @books = Book.where("title LIKE ?", "#{keyword}%")
+      @books = Book.where("title LIKE ?", "#{@keyword}%")
     when 'partial_match' then
-      @books = Book.where("title LIKE ?", "%#{keyword}%")
+      @books = Book.where("title LIKE ?", "%#{@keyword}%")
     end
 
-  else kind == 'user'
+  elsif @kind == 'user'
     case match_type
     when 'perfect_match' then
-      @users = User.where(title: keyword)
+      @users = User.where("name LIKE ?", "#{@keyword}")
     when 'forward_match' then
-      @users = User.where("title LIKE ?", "%#{keyword}")
+      @users = User.where("name LIKE ?", "%#{@keyword}")
     when 'backword_match' then
-      @users = User.where("title LIKE ?", "#{keyword}%")
+      @users = User.where("name LIKE ?", "#{@keyword}%")
     when 'partial_match' then
-      @users = User.where("title LIKE ?", "%#{keyword}%")
+      @users = User.where("name LIKE ?", "%#{@keyword}%")
     end
   end
 end
+
 
 end
