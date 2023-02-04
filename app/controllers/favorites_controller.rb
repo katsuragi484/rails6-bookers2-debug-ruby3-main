e0e8cf6a5c61052823b70ef127ac6c5d8c747760
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-02-04 13:40:12 +0000

    [Modify]いいね機能_destroy

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,8 @@
 class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.new(book_id: @book.id)
-    @favorite.save
+    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite.save
     # render 'create'
     # redirect_to request.referer
   end
