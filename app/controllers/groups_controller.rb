42e9fc5d8961129956488ae483a6c3b4ffd64120
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-02-18 03:34:16 +0000

    [Add]create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,18 @@ class GroupsController < ApplicationController
     @user = current_user
     @book = Book.new
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      # redirect_to
+    else
+      render "new"
+    end
+  end
+
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,5 @@ class Group < ApplicationRecord
   has_many :group_user
   has_many :user, through: :group_user
 
+  has_one_attached :image
 end
